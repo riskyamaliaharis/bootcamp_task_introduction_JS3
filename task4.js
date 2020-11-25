9cc2d3e6ba22ac7e2b3b5c3bd4f680bde7e952b7
Risky Amalia Haris <amaliaharisr@gmail.com>
2020-11-26 05:31:21 +0700

    add try catch

--- task4.js
+++ task4.js
@@ -1,9 +1,12 @@
 async function getName() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await response.json()    
-    data.forEach((elemen)=> console.log(elemen.name))
+    try{    
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()    
+        data.forEach((elemen)=> console.log(elemen.name)) 
+    } catch (error){
+        console.log(error)
+    }   
 }
 
 getName()
-
 
